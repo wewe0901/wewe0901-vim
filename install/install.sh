e54a462719b7a3e6e1bbed2b3530acbc674c12bc
wewe0901 <wewe0901@gmail.com>
2012-07-26 15:20:30 +0800

    * install pathongen * add install script * add basic vimrc

---
+++ install/install.sh
@@ -0,0 +1,12 @@
+
+[ -e "~/.vim:" ] && echo "~/.vim already exists please remove first." && exit 1
+
+git clone git://github.com/wewe0901/wewe0901-vim.git ~/.vim
+
+cd ~/.vim 
+
+git submodule update --init
+
+ln -s  ~/.vim/vimrc ~/.vimrc
+
+echo "vimrc is installed."
